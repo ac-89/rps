ce641e1135680474ae64af3652d53ba01af00ebb
ac-89 <alancarroll89@gmail.com>
2022-07-13 13:19:23 +0100

    Create scoreCheck function, score variables and fix refresh bug
    Score variables keep track of player scores.
    Every win increments the score variables.
    If either score reaches 5, the winner is printed to the console and the game ends.
    fix refresh bug where refreshing the page would add to the players score.
    Add if statement to gameCheck player victory section where the score will not increment if the player value is null.

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 let player;
-let computer;
+let computer = null;
+let playerScore = 0;
+let computerScore = 0;
 
 let computerPlay = function () {
   let randNum = Math.floor(Math.random() * 3);
   let plays = ["rock", "paper", "scissors"];
   computer = plays[randNum];
+  console.log(`Player score is ${playerScore}`);
+  console.log(`Computer score is ${computerScore}`);
 };
 
 let gameCheck = function () {
   if (player == computer) {
     console.log("it's a draw");
+    scoreCheck();
   } else if (
     (computer == "rock" && player == "scissors") ||
     (computer == "scissors" && player == "paper") ||
     (computer == "paper" && player == "rock")
   ) {
     console.log("Computer is the winner!");
+    computerScore++;
+    scoreCheck();
   } else {
-    console.log("player wins!");
+    console.log("player wins this round!");
+    if (player != null) {
+      playerScore++;
+    }
+    scoreCheck();
+  }
+};
+
+let scoreCheck = function () {
+  if (playerScore == 5) {
+    console.log("Player wins!");
+  } else if (computerScore == 5) {
+    console.log("Computer wins!");
+  } else {
+    computerPlay();
+    playerChoice();
   }
 };
 
